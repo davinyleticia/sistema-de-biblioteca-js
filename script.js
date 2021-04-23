a49e9221ca66e09b660f990980cd444211d5c53d
Daviny Vidal <daviny.vidal@outlook.com>
2021-04-22 21:23:00 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function showData(event) {
+    event.preventDefault();
+
+    let setAutor = document.getElementById('autor').value;
+    let setTitulo = document.getElementById('Titulo').value;
+    let setISBN = document.getElementById('ISBN').value;
+    let setPublicacao = document.getElementById('Publicacao').value;
+    let setDate = new Date().toLocaleDateString('pt-br');
+    let setSchedule = new Date().toLocaleTimeString('pt-br');
+
+    document.getElementById('resposta').innerHTML += `
+
+    <ul>
+        <li>
+            <strong>Autor:</strong> ${setAutor}, <strong>Título:</strong> ${setTitulo},
+            <strong>ISBN:</strong> ${setISBN}, <strong>Publicaçao:</strong> ${setPublicacao},
+            <strong>Data de Inserção:</strong> ${setDate}, ${setSchedule}, <br><br>
+            <a href="#" class="delete">Deletar</a> 
+            <br><br>
+        </li>
+    </ul>
+    `
+    clearDate()
+}
+
+function clearDate(){
+
+    let setAutor = document.getElementById('autor').value = "";
+    let setTitulo = document.getElementById('Titulo').value = "";
+    let setISBN = document.getElementById('ISBN').value = "";
+    let setPublicacao = document.getElementById('Publicacao').value = "";
+}
+
+document.getElementById('resposta').addEventListener('click', function(event){
+
+    if(event.target.className === "delete"){
+        event.target.parantElement.remove();
+    }
+})
